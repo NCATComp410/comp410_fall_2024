364e68ef11b05a8a659a564aaf7452baab8aa83e
Jaden Nobles <jmnobles1@aggies.ncat.edu>
2024-10-26 20:49:15 +0000

    es_nie test case

--- test_team_1.py
+++ test_team_1.py
@@ -11,6 +11,26 @@ class TestTeam_1(unittest.TestCase):
 
     def test_es_nie(self):
         """Test ES_NIE functionality"""
+        country_codes = ['X', 'Y', 'Z']  # Starting letters for NIEs
+        digits = ['9613851', '8063915', '8078221']  # 7 digits for NIE
+        control_letters = ['N', 'Z', 'M']  # Control letters
+
+        nie_codes = []
+        for i, country_code in enumerate(country_codes):
+            nie = f"{country_code}{digits[i]}{control_letters[i]}"
+            nie_codes.append(nie)
+
+        for nie in nie_codes:
+            result = analyze_text('NIE is ' + nie, ['ES_NIE'])
+            self.assertEqual('ES_NIE', result[0].entity_type)
+            print('\n' + nie)
+            print(result)
+
+        # Negative test case
+        nie = 'W1234567X'  # Invalid NIE (wrong starting letter)
+        result = analyze_text('NIE is ' + nie, ['ES_NIE'])
+        self.assertListEqual([], result)
+        print('\n', result)
 
     def test_es_nif(self):
         """Test ES_NIF functionality"""
