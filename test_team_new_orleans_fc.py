17cbb34678a855b513a9a43ddb9a9f967242c335
claesmk <claesmk@gmail.com>
2024-10-28 22:03:10 -0400

    Corrected test_email_address_negatives() so that it will run

--- test_team_new_orleans_fc.py
+++ test_team_new_orleans_fc.py
@@ -18,8 +18,8 @@ class TestTeam_new_orleans_fc(unittest.TestCase):
     def test_date_time(self):
         """Test DATE_TIME functionality"""
 
-        """Test EMAIL_ADDRESS functionality"""
     def test_email_address_positives(self):
+        """Test EMAIL_ADDRESS positive functionality"""
         test_cases = {
             "Standard email": "user@example.com",
             "Email with subdomain": "user@mail.example.com",
@@ -34,21 +34,22 @@ class TestTeam_new_orleans_fc(unittest.TestCase):
                 self.assertTrue(result, "Analyzer detected no email")
                 self.assertEqual("EMAIL_ADDRESS", result[0].entity_type)
 
-        def test_email_address_negatives(self):
-            """Tests that are non-email strings are not detected as EMAIL_ADDRESS type"""
+    def test_email_address_negatives(self):
+        """Tests that are non-email strings are not detected as EMAIL_ADDRESS type"""
 
-            test_cases = {
-                "Phone number": "336-291-1191",
-                "Random text": "Just some random text",
-                "Date format": "10/28/2024",
-                "Alphanumeric string": "ABCD1234XYZ",
-            }
+        test_cases = {
+            "Phone number": "336-291-1191",
+            "Random text": "Just some random text",
+            "Date format": "10/28/2024",
+            "Alphanumeric string": "ABCD1234XYZ",
+        }
 
-            for test_description, test_input in test_cases.items():
-                with self.subTest(msg=test_description, test_input=test_input):
-                    result = analyze_text(test_input, entity_list=["EMAIL_ADDRESS"])
+        for test_description, test_input in test_cases.items():
+            with self.subTest(msg=test_description, test_input=test_input):
+                result = analyze_text(test_input, entity_list=["EMAIL_ADDRESS"])
 
-                    self.assertFalse(result, "Analyzer incorrectly detected an email address")
+                self.assertFalse(result, "Analyzer incorrectly detected an email address")
+
     def test_medical_license(self):
         """Test MEDICAL_LICENSE functionality"""
 
