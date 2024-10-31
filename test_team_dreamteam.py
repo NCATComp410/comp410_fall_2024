bf9f43df4481144a04b3ad7400261d030c29ddf7
AmariBull <159070930+AmariBull@users.noreply.github.com>
2024-10-31 21:29:50 +0000

    Added AU_ACN Unit Tests

--- test_team_dreamteam.py
+++ test_team_dreamteam.py
@@ -1,6 +1,6 @@
 """Unit test file for team dreamteam"""
 import unittest
-from pii_scan import analyze_text, show_aggie_pride  # noqa 
+from pii_scan import analyze_text, show_aggie_pride  # noqa
 
 
 class TestTeam_dreamteam(unittest.TestCase):
@@ -17,6 +17,30 @@ class TestTeam_dreamteam(unittest.TestCase):
 
     def test_au_acn(self):
         """Test AU_ACN functionality"""
+        pre = ['123']
+        mid = ['456']
+        suf = ['785']
+
+        # positive test cases
+        for p in pre:
+            for m in mid:
+                for s in suf:
+                    # check context score
+                    acn = '-'.join([p,m,s])
+                    print(acn)
+                    result = analyze_text('My ACN is ' + acn, ['AU_ACN'])
+                    print(result)
+                    self.assertEqual('AU_ACN', result)
+                    self.assertEqual(0.1, result[0].score)
+
+                    # checks no context
+                    result = analyze_text('My ACN is ' + acn, ['AU_ACN'])
+                    self.assertEqual('AU_ACN', result)
+                    self.assertEqual(0.01, result[0].score)
+
+        # negative test cases
+        result_invalid = analyze_text('My ACN is 123-456-789', ['AU-ACN'])
+        self.assertEqual([], result_invalid)
 
     def test_au_medicare(self):
         """Test AU_MEDICARE functionality"""
