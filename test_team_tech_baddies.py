aaf637af3cc3af7830cff43f70edeb1650f6bae9
claesmk <claesmk@gmail.com>
2024-11-03 18:07:39 -0500

    Added back test_in_pan

--- test_team_tech_baddies.py
+++ test_team_tech_baddies.py
@@ -46,9 +46,25 @@ class TestTeam_tech_baddies(unittest.TestCase):
         print("Negative test result:", result)
         self.assertListEqual([], result)
 
+    def test_in_pan(self):
+        """Test IN_PAN functionality"""
+        # Positive test cases
+        # check context
+        result = analyze_text('My IN_PAN is AAAPZ1234C', ['IN_PAN'])
+        print(result)
+        self.assertEqual('IN_PAN', result[0].entity_type)
+        self.assertEqual(1.0, result[0].score)
 
+        # check no context
+        result = analyze_text('My IN_PAN is 12APZ1234C', ['IN_PAN'])
+        print(result)
+        self.assertEqual('IN_PAN', result[0].entity_type)
+        self.assertEqual(0.4, result[0].score)
 
-        
+        # Negative test cases
+        result = analyze_text('My IN_PAN is 0000000000', ['IN_PAN'])
+
+        self.assertListEqual([], result)
 
     def test_in_passport(self):
         """Test IN_PASSPORT functionality"""
@@ -72,7 +88,6 @@ class TestTeam_tech_baddies(unittest.TestCase):
         # negative test case - no result found
         result = analyze_text('My passport number is A00000000',['IN_PASSPORT'])
         self.assertListEqual([], result)
-
 
     def test_in_vehicle_registration(self):
         """Test IN_VEHICLE_REGISTRATION functionality"""
